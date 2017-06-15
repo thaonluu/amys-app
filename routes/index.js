1d55139689d2707fc0bc8971013b8f80fa3a0087
Thao <tluu4@mail.sfsu.edu>
2017-06-15 00:55:30 -0700

    switched to hbs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render ('index', { title: "HELLO!" });
 });
+
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
+});
+
 
 module.exports = router;

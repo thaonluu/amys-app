d3d8d9356fe1e315f95a3e70acbe91a20d967b12
Thao <tluu4@mail.sfsu.edu>
2017-06-15 01:06:00 -0700

    switched to hbs

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// router.get('/users', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
 module.exports = router;

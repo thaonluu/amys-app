92bbc5f38421e216da7b2cd403f46fd0ddc0f1fb
Thao <tluu4@mail.sfsu.edu>
2017-06-26 17:51:48 -0700

    my skills section completed, scrollspy offset, new landing img, social media buttons

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'thaos site'});
+  res.render('index', { title: 'Thao Luu | Front-End Web Developer'});
 });
 
 router.get('/users', function(req, res, next) {
